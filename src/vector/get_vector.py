bbf73b2b3802ccda658b16a1eed951a2b0f63c45
SKKUGoon <baesangil0906@gmail.com>
2025-06-25 02:41:45 +0900

    add: drug-ingredients

--- src/vector/get_vector.py
+++ src/vector/get_vector.py
@@ -81,3 +81,82 @@ def get_outpatient_vectors(client: QdrantClient, collection_name: str, year: int
         vector.append(i.vector)
 
     return pd.DataFrame(meta), np.array(vector)
+
+
+def get_inpatient_vectors_ing(client: QdrantClient, collection_name: str, year: int) -> Tuple[pd.DataFrame]:
+    print("loading inpatient vectors for year", year)
+
+    # Data is too big. Divide the data by year
+    vectors = client.scroll(
+        collection_name=collection_name,
+        limit=80000,  # Adjust limit based on your data size
+        with_vectors=True,  # Include embedding vectors,
+        scroll_filter={
+            "must": [
+                {
+                    "key": "year", 
+                    "match": {"value": year}
+                }
+            ]
+        }
+    )[0]
+
+    # Meta data & Vector data
+    meta = []
+    vector = []
+
+    for i in vectors:
+        meta.append(
+            [
+                i.id, 
+                i.payload['patient_id'], 
+                i.payload['visit_date'],
+                i.payload['patient_type'], 
+                i.payload['patient_sex'],  
+                i.payload['patient_age'],  
+                i.payload['patient_major_code'], 
+                i.payload['patient_minor_code'],
+                i.payload['patient_medicine_ingredients']
+            ]
+        )
+        vector.append(i.vector)
+
+    return pd.DataFrame(meta), np.array(vector)
+
+
+def get_outpatient_vectors_ing(client: QdrantClient, collection_name: str, year: int) -> Tuple[pd.DataFrame]:
+    print("loading outpatient vectors for year", year)
+
+    # Data is too big. Divide the data by year
+    vectors = client.scroll(
+        collection_name=collection_name,
+        limit=80000,  # Adjust limit based on your data size
+        with_vectors=True,  # Include embedding vectors,
+        scroll_filter={
+            "must": [
+                {
+                    "key": "year", 
+                    "match": {"value": year}
+                }
+            ]
+        }
+    )[0]
+
+    # Meta data & Vector data
+    meta = []
+    vector = []
+
+    for i in vectors:
+        meta.append([
+            i.id, i.payload['patient_id'], 
+            i.payload['visit_date'], 
+            i.payload['patient_type'], 
+            i.payload['patient_sex'], 
+            i.payload['patient_age'], 
+            i.payload['patient_major_code'], 
+            i.payload['patient_minor_code'], 
+            i.payload['patient_medicine_ingredients']
+        ])
+        vector.append(i.vector)
+
+    return pd.DataFrame(meta), np.array(vector)
